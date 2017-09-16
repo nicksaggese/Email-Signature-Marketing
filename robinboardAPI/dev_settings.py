af95e5c1e5d446aad42c07d6559be5144db7d4e6
Nicholas Saggese <nfsaggese@gmail.com>
2017-09-15 22:32:19 -0400

    dev settings

---
+++ robinboardAPI/dev_settings.py
@@ -0,0 +1,13 @@
+# Start with our base settings
+from .settings import *
+DEBUG=True
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': "djangorest",
+            'USER': "robinboardDev",
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': "robinboard-dev.cng93thgqnqn.us-east-1.rds.amazonaws.com",
+            'PORT': "5432",
+        }
+}
